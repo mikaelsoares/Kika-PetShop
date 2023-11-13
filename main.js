5a59d1311c395df2b7e64c4a931438b447baaf5a
Guilherme Koerber <44475998+guilherme-koerber@users.noreply.github.com>
2023-11-13 20:55:25 -0300

    Update main.js
    Co-authored-by: Mikael <101341760+mikaelsoares@users.noreply.github.com>

--- main.js
+++ main.js
@@ -3,5 +3,4 @@ const botao = document.querySelector("#scrollButton");
 botao.addEventListener("click", () => {
   const alturaTotalPagina = document.body.scrollHeight;
   window.scroll({ top: alturaTotalPagina, behavior: "smooth" });
-  // botao.style.display = "none";
 });
